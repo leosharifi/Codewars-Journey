c81d98b58c94b89aa10409b2874a73ddd181bbfb
Elyas Sharifi <elyassharifi2548@gmail.com>
2025-01-07 19:06:45 +0430

    8 Kyu
    Return "hello world!"

---
+++ 8 Kyu/2.  Function 1 - hello world.js
@@ -0,0 +1,8 @@
+// Write a function "greet" that returns "hello world!"
+function greet() {
+    return 'hello world!'
+}
+
+
+
+// const greet = () => "hello world!"; //this is the other way you can do it.
