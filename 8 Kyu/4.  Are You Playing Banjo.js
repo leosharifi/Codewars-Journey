02977e1fd4c9be9586c9ca20dc24c20e8b5d1df6
Elyas Sharifi <elyassharifi2548@gmail.com>
2025-01-07 19:09:25 +0430

    8 Kyu
    if the first letter of your name starts with R then it is true
    else it is false

---
+++ 8 Kyu/4.  Are You Playing Banjo.js
@@ -0,0 +1,9 @@
+function areYouPlayingBanjo(name) {
+    // Implement me
+    if(name[0].toUpperCase() == 'R'){
+      return `${name} plays banjo`
+    }else {
+      return `${name} does not play banjo`
+    }
+    return name;
+}
