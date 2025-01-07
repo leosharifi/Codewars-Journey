f3d9bd2d62bf5354ec40b2a798d3be67d0ca9d5f
Elyas Sharifi <elyassharifi2548@gmail.com>
2025-01-07 21:34:45 +0430

    8 kyu
    How to find a word in an array based on it's position

---
+++ 8 Kyu/12.  A Needle in the Haystack.js
@@ -0,0 +1,8 @@
+function findNeedle(haystack) {
+    // your code here
+    let sum = haystack.indexOf('needle');
+    return `found the needle at position ${sum}`;
+}
+
+
+//.indexOf() //it find the position in an array;
